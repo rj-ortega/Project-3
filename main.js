67211251df7465698089f8391a66332b0997b1ba
rjortega <rikrdo.ortega@gmail.com>
2017-03-09 21:58:25 -0700

    Fixed JS
    removed eventListener.remove

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 
-// replace .hide within the HTML
- document.getElementById("joke-part2").classList.add("hide");
- document.getElementById("button2").classList.add("hide");
- document.getElementById("joke-part3").classList.add("hide");
-
 // When user clicks first button, show second part
 document.getElementById("button1").addEventListener("click", function() {
-  document.getElementById("joke-part2").classList.add("show");
-  document.getElementById("button2").classList.add("show");
-  document.getElementById("joke-part1").classList.add('hide');
-  document.getElementById("button1").classList.add('hide');
+  document.getElementById("joke-part1").classList.add("hide");
+  document.getElementById("button1").classList.add("hide");
+  document.getElementById("button2").classList.remove("hide");
+  document.getElementById("joke-part2").classList.remove("hide");
 });
 
 document.getElementById("button2").addEventListener("click", function() {
-  document.getElementById("joke-part3").classList.add("show");
+  document.getElementById("joke-part3").classList.remove("hide");
   document.getElementById("joke-part2").classList.add("hide");
   document.getElementById("button2").classList.add("hide");
 });
